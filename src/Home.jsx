2a26e1cd92aed06fde9deaecda6b615b0c91f62c
rima-das2004 <rima983131@gmail.com>
2025-07-21 04:33:16 +0530

    first commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,7 @@ muted
 loop
  playsInline
 className="absolute top-0 left-0 w-full h-full object-cover z-0 blur-sm">
-<source src="/bg-1.mp4" type="video/mp4" />
+<source src="bg-1.mp4" type="video/mp4" />
  Your browser does not support the video tag.
 </video>
 
