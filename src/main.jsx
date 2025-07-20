66227c1323060c4ccc93536eb3e0675c251456f9
rima-das2004 <rima983131@gmail.com>
2025-07-21 04:52:21 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-<React.StrictMode>
-  <HashRouter>
-    <App />
-  </HashRouter>
-</React.StrictMode>
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename='/'>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
