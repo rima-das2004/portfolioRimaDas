006937184c9a9716893a26ea2ac4fd3d6d7bcd1f
rima-das2004 <rima983131@gmail.com>
2025-07-21 04:12:48 +0530

    first commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-});
+  base: '/', // VERY IMPORTANT for Render static hosting
+})
